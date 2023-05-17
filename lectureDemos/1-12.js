4038c875695226bc403c24dced496708acaf4f1b
Jams Jams <yarinyroblero@Jamyss-iMac.local>
2023-05-17 12:21:57 -0400

    updates

---
+++ lectureDemos/1-12.js
@@ -0,0 +1,12 @@
+let kids = [
+    { first : "Natalie", last : "Plyers" },
+    { first: "Brittany", last: "Ray" },
+    { first: "Zachary", last: "Westly" }
+  ];
+ kids.forEach(displayKid)
+
+
+  function displayKid(kidToDisplay) {
+    console.log(kidToDisplay.first + " " + kidToDisplay.last);
+  } 
+  
---
+++ lectureDemos/ages.js
@@ -0,0 +1,7 @@
+let ages = [3, 10, 18, 20];
+
+
+function checkAge(ages) {
+  return ages > 18;
+}
+console.log(checkAge(ages));
